3c7b217958706d66c6be450b934b975430e0ff86
Thiviya Sivasthamparam <vakesensivasthamparam@thiviyas-air.lan>
2022-10-07 20:06:39 -0400

    updated github profile link

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,20 +1,112 @@
+const licenseUrl = {
+  'apache-2.0': "https://opensource.org/licenses/Apache-2.0",
+  'bsd-3-clause': "https://opensource.org/licenses/BSD-3-Clause",
+  'epl-1.0': "https://opensource.org/licenses/EPL-1.0",
+  'gpl-3.0': "https://www.gnu.org/licenses/gpl-3.0",
+  'mit': "https://opensource.org/licenses/MIT",
+  'mpl-2.0': "https://opensource.org/licenses/MPL-2.0",
+  'wtfpl': "http://www.wtfpl.net/about/"
+}
+
+const licenseBadge = {
+  'apache-2.0': "https://img.shields.io/badge/License-Apache_2.0-blue.svg",
+  'bsd-3-clause': "https://img.shields.io/badge/License-BSD_3--Clause-blue.svg",
+  'epl-1.0': "https://img.shields.io/badge/License-EPL_1.0-red.svg",
+  'gpl-3.0': "https://img.shields.io/badge/License-GPLv3-blue.svg",
+  'mit': "https://img.shields.io/badge/License-MIT-yellow.svg",
+  'mpl-2.0': "https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg",
+  'wtfpl': "https://img.shields.io/badge/License-WTFPL-brightgreen.svg"
+}
+
 // TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+
+function renderLicenseBadge(licenseName) {
+  return licenseBadge[licenseName];
+  /*
+  let obj = {
+    prop: value,
+  };
+  obj["key2"] = "name";
+  console.log(obj.prop);
+  console.log(obj['prop']);
+  let keyName = 'prop';
+  console.log(obj[keyName]);
+    => console.log(obj['prop']);
+  */
+
+}
+
+
 
 // TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+
+function renderLicenseLink(license) {
+  return licenseUrl[license];
+}
+
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license === "none") {
+    return '';
+  }
+  let badgeUrl = renderLicenseBadge(license);
+  let licenseLink = renderLicenseLink(license);
 
-// TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+  return `## License
+  [![License:${license}](${badgeUrl})](${licenseLink})
+  `;
 
-`;
-}
+};
+
+
+const generateMarkdown = ({ title, description,
+  installation, usage, contributionGuidelines,
+  testInstructions, license, githubUserName, email }) =>
+  `# ${title}
+
+## Description
+ ##### ${description}
+
+ # Table of contents
+[Description](#description)
+    
+[Installation](#installation)
+    
+[Usage](#usage)
+    
+[Contribution Guidelines](#contributionguidelines)
+    
+[Test Instructions](#testinstructions)
+    
+${license === "none" ? "" : "[License](#license)"}
+    
+    
+## Installation
+${installation}
+    
+## Usage
+${usage}
+    
+## Contribution Guidelines
+###### ${contributionGuidelines}
+    
+    
+## Test Instructions
+###### ${testInstructions}
+    
+## Github Username
+##### <a href="https://github.com/${githubUserName}/">Click here to go to my Github repository page.</a></br>
+
+## Email
+##### ${email}
+    
+${renderLicenseSection(license)}`;
+
 
 module.exports = generateMarkdown;
+
+// to take input and replace in correct spots
+//makes license coniditonal show or not
+//license table of content shown if its there or not
--- index.js
+++ index.js
@@ -1,13 +1,117 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require("./generateMarkdown");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+inquirer.prompt([
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is your project title?',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'What is the description for your project?',
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'What are the instructions to install your project?',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'How do you use your project?',
+    },
+    {
+        type: 'input',
+        name: 'contributionGuidelines',
+        message: 'How do others contribute to your project?',
+    },
+    {
+        type: 'input',
+        name: 'testInstructions',
+        message: 'What are the test instructions?',
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'What type of license does this project have?',
+        choices: [
+            {
+                name: "Apache 2.0 License",
+                value: "apache-2.0",
+            },
+            {
+                name: "BSD 3-Clause License",
+                value: "bsd-3-clause",
+            },
+            {
+                name: "Eclipse Public License 1.0",
+                value: "epl-1.0",
+            },
+            {
+                name: "GNU GPL v3",
+                value: "gpl-3.0",
+            },
+            {
+                name: "The MIT License",
+                value: "mit",
+            },
+            {
+                name: "Mozilla Public License 2.0",
+                value: "mpl-2.0",
+            },
+            {
+                name: "The Do What the Fuck You Want to Public License",
+                value: "wtfpl",
+            },
+            {
+                name: "No License",
+                value: "none",
+            }
+        ]
+    },
+    {
+        type: 'input',
+        name: 'githubUserName',
+        message: 'What is your github username?',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address?',
+    },
+])
+    .then((answers) => {
+        console.log(answers)
+        writeToFile(`${answers.title}-readme.md`, generateMarkdown(answers));
+    });
+
+
+
+
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) =>
+        err ? console.log(err) : console.log(`Successfully created ${fileName}!`)
 
-// TODO: Create a function to initialize app
-function init() {}
+    );
+}
 
-// Function call to initialize app
-init();
+
+
+
+
+// when using template literals use ticks, not quotes or singe quotes
+// let name = "abc";
+// "hi my name is " + name + "!"
+// 'hi my name is ' + name + '!'
+//     `hi my name is ${name}`
+
+
+
+
